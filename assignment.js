4f2831cdfc34d5edecc999dcd3d963a2e36b0ddc
Defy1x <mcdonald.cas@yahoo.com>
2019-08-11 04:12:44 -0400

    Ready for submission

--- assignment.js
+++ assignment.js
@@ -32,6 +32,14 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+let result =''
+let j = 2
+
+for(let i=0;i<=12;i++){
+    result += (i * j)+ ' ';
+}
+console.log(result)
+
 // Add your code above this line
 
 console.log('')
@@ -65,6 +73,13 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
+let values = [57,13,4,38]
+
+console.log(Math.max.apply(null, values))
+
+let valuesTwo = [3,48,29,12,18]
+
+console.log(Math.max.apply(null, valuesTwo))
 
 // Add your code above this line
 
@@ -112,6 +127,131 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+
+//GAME 1
+let playerAChoiceOne = 'rock'
+let playerBChoiceOne = 'paper'
+
+  if (playerAChoiceOne === playerBChoiceOne) {
+    console.log('It is a tie yo!')
+  }
+  if (playerAChoiceOne === 'rock') {
+    if (playerBChoiceOne === 'paper') {
+      // Paper beats Rock
+      console.log('Player B wins!')
+    }
+    else {
+      // Rock beats scissors
+      console.log('Player A wins!')
+    }
+  }
+
+  if (playerAChoiceOne === 'paper') {
+    if (playerBChoiceOne === 'scissors') {
+      // Scissors beats paper
+      console.log('Player B wins!')
+    }
+    else {
+      // Paper beats rock
+      console.log('Player A wins!')
+    }
+  }
+
+  if (playerAChoiceOne === 'scissors') {
+    if (playerBChoiceOne === 'rock') {
+      // Rock beats scissors
+      console.log('Player B wins!')
+    }
+    else {
+      // Scissors beats paper
+      console.log('Player A wins!')
+    }
+}
+
+// GAME 2
+
+let playerAChoiceTwo = 'scissors'
+let playerBChoiceTwo = 'scissors'
+
+  if (playerAChoiceTwo === 'rock') {
+    if (playerBChoiceTwo === 'paper') {
+      // Paper beats Rock
+      console.log('Player B wins!')
+    }
+    if (playerAChoiceTwo === playerBChoiceTwo) {
+      console.log('It is a tie yo!')
+    }
+    else {
+      // Rock beats scissors
+      console.log('Player A wins!')
+    }
+  }
+  if (playerAChoiceTwo === 'paper') {
+    if (playerBChoiceTwo === 'scissors') {
+      // Scissors beats paper
+      console.log('Player B wins!')
+    }
+    if (playerAChoiceTwo === playerBChoiceTwo) {
+      console.log('It is a tie yo!')
+    }
+    else {
+      // Paper beats rock
+      console.log('Player A wins!')
+    }
+  }
+  if (playerAChoiceTwo === 'scissors') {
+    if (playerBChoiceTwo === 'rock') {
+      // Rock beats scissors
+      console.log('Player B wins!')
+    }
+    if (playerAChoiceTwo === playerBChoiceTwo) {
+      console.log('It is a tie yo!')
+    }
+    else {
+      // Scissors beats paper
+      console.log('Player A wins!')
+    }
+}
+
+// Game 3
+
+let playerAChoiceThree ="scissors"
+let playerBChoiceThree ="paper"
+
+  if (playerAChoiceThree === playerBChoiceThree) {
+    console.log('It is a tie yo!')
+  }
+  if (playerAChoiceThree === 'rock') {
+    if (playerBChoiceThree === 'paper') {
+      // Paper beats Rock
+      console.log('Player B wins!')
+    }
+    else {
+      // Rock beats scissors
+      console.log('Player A wins!')
+    }
+  }
+  if (playerAChoiceThree === 'paper') {
+    if (playerBChoiceThree === 'scissors') {
+      // Scissors beats paper
+      console.log('Player B wins!')
+    }
+    else {
+      // Paper beats rock
+      console.log('Player A wins!')
+    }
+  }
+  if (playerAChoiceThree === 'scissors') {
+    if (playerBChoiceThree === 'rock') {
+      // Rock beats scissors
+      console.log('Player B wins!')
+    }
+    else {
+      // Scissors beats paper
+      console.log('Player A wins!')
+    }
+}
+
 
 // Add your code above this line
 
